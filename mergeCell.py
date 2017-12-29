071e74414e2fbcca11202640f7a65adcaa10828e
Frank Yang <yangpoan@gmail.com>
2017-12-29 09:54:25 +0800

    add main in mergeCell.py

--- mergeCell.py
+++ mergeCell.py
@@ -13,6 +13,7 @@ def mergeCellsVertically(table, start_row_idx, end_row_idx, col_idx):
     for c in column_cells[1:]:
         c._tc.set('vMerge', '1')
 
+
 # merge cells horizontally
 def mergeCellsHorizontally(table, row_idx, start_col_idx, end_col_idx):
     col_count = end_col_idx - start_col_idx + 1
@@ -21,6 +22,7 @@ def mergeCellsHorizontally(table, row_idx, start_col_idx, end_col_idx):
     for c in row_cells[1:]:
         c._tc.set('hMerge', '1')
 
+
 # the workaround function to merge cells in a table
 def mergeCells(table, start_row_idx, end_row_idx, start_col_idx, end_col_idx):
     for col_idx in range(start_col_idx,end_col_idx+1):
@@ -28,46 +30,48 @@ def mergeCells(table, start_row_idx, end_row_idx, start_col_idx, end_col_idx):
     for row_idx in range(start_row_idx,end_row_idx+1):
         mergeCellsHorizontally(table, row_idx, start_col_idx, end_col_idx)
 
-prs = Presentation()
-title_only_slide_layout = prs.slide_layouts[5]
-slide = prs.slides.add_slide(title_only_slide_layout)
-shapes = slide.shapes
 
-shapes.title.text = 'Adding a Table'
+if __name__ == '__main__':
+    prs = Presentation()
+    title_only_slide_layout = prs.slide_layouts[5]
+    slide = prs.slides.add_slide(title_only_slide_layout)
+    shapes = slide.shapes
 
-rows = cols = 4
-top = Inches(2.0)
-left = Inches(1.0)
-width = Inches(8.0)
-height = Inches(1)
+    shapes.title.text = 'Adding a Table'
 
-table = shapes.add_table(rows, cols, left, top, width, height).table
+    rows = cols = 4
+    top = Inches(2.0)
+    left = Inches(1.0)
+    width = Inches(8.0)
+    height = Inches(1)
 
-# set column widths
-table.columns[0].width = Inches(2.0)
-table.columns[1].width = Inches(2.0)
-table.columns[2].width = Inches(2.0)
-table.columns[3].width = Inches(2.0)
+    table = shapes.add_table(rows, cols, left, top, width, height).table
 
-# write column headings
-table.cell(0, 0).text = 'Column1'
-table.cell(0, 1).text = 'Column2'
-table.cell(0, 2).text = 'Column3'
-table.cell(0, 3).text = 'Column4'
+    # set column widths
+    table.columns[0].width = Inches(2.0)
+    table.columns[1].width = Inches(2.0)
+    table.columns[2].width = Inches(2.0)
+    table.columns[3].width = Inches(2.0)
 
-# write body cells
-table.cell(1, 0).text = 'Merged vertically and horizontally'
-table.cell(1, 2).text = 'Cell 1-3'
-table.cell(2, 2).text = 'Cell 2-3'
-table.cell(3, 2).text = 'Cell 3-3'
-table.cell(1, 3).text = 'Merged vertically'
-table.cell(3, 0).text = 'Merged horizontally'
-table.cell(3, 3).text = 'Cell 3-4'
+    # write column headings
+    table.cell(0, 0).text = 'Column1'
+    table.cell(0, 1).text = 'Column2'
+    table.cell(0, 2).text = 'Column3'
+    table.cell(0, 3).text = 'Column4'
 
-# merge cells vertically
-mergeCellsVertically(table=table, start_row_idx=1, end_row_idx=2, col_idx=3)
-# merge cells horizontally
-mergeCellsHorizontally(table=table, row_idx=3, start_col_idx=0, end_col_idx=1)
-# merge cells vertically and horizontally
-mergeCells(table=table, start_row_idx=1, end_row_idx=2, start_col_idx=0, end_col_idx=1)
-prs.save('test.pptx')
+    # write body cells
+    table.cell(1, 0).text = 'Merged vertically and horizontally'
+    table.cell(1, 2).text = 'Cell 1-3'
+    table.cell(2, 2).text = 'Cell 2-3'
+    table.cell(3, 2).text = 'Cell 3-3'
+    table.cell(1, 3).text = 'Merged vertically'
+    table.cell(3, 0).text = 'Merged horizontally'
+    table.cell(3, 3).text = 'Cell 3-4'
+
+    # merge cells vertically
+    mergeCellsVertically(table=table, start_row_idx=1, end_row_idx=2, col_idx=3)
+    # merge cells horizontally
+    mergeCellsHorizontally(table=table, row_idx=3, start_col_idx=0, end_col_idx=1)
+    # merge cells vertically and horizontally
+    mergeCells(table=table, start_row_idx=1, end_row_idx=2, start_col_idx=0, end_col_idx=1)
+    prs.save('test.pptx')
